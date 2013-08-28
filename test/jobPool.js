5bec7ce60a8cfe7d56f78d7262a1d34e8d5853f4
miniflycn <miniflycn@justany.net>
2013-08-28 23:13:15 +0800

    tab to 2 space
    tab to 2 space

--- test/jobPool.js
+++ test/jobPool.js
@@ -3,22 +3,22 @@ var assert = require('assert')
 	, jobPool = require('../lib/jobPool');
 
 describe('jobPool', function () {
-	it('should able to cache job', function () {
-		var job1 = Job.fetch('http://localhost/test1')
-			, job2 = Job.fetch('http://localhost/test2')
-			, job3 = Job.fetch('http://localhost/test3')
-			, jobList;
-		jobPool.push(job1);
-		jobPool.push(job2);
-		jobPool.push(job3);
-		jobList = jobPool.shift(3);
-		jobList.length.should.equal(3);
-	});
+  it('should able to cache job', function () {
+    var job1 = Job.fetch('http://localhost/test1')
+    , job2 = Job.fetch('http://localhost/test2')
+    , job3 = Job.fetch('http://localhost/test3')
+    , jobList;
+    jobPool.push(job1);
+    jobPool.push(job2);
+    jobPool.push(job3);
+    jobList = jobPool.shift(3);
+    jobList.length.should.equal(3);
+  });
 
-	it('should able to get a job', function () {
-		var job = Job.fetch('http://localhost/test4');
-		jobPool.push(job);
-		jobPool.shift(1);
-		jobPool.get('http://localhost/test4').should.equal(job);
-	});
+  it('should able to get a job', function () {
+    var job = Job.fetch('http://localhost/test4');
+    jobPool.push(job);
+    jobPool.shift(1);
+    jobPool.get('http://localhost/test4').should.equal(job);
+  });
 });
