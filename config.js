a95ffe9c2e048ae20e6aec955d873327dabc3fe5
miniflycn <miniflycn@justany.net>
2013-09-14 21:57:27 +0800

    Release 0.0.8
    Release 0.0.8

--- config.js
+++ config.js
@@ -7,14 +7,14 @@ module.exports = {
   maxQueueJob: 400,
   maxCache: 10000,
   viewportSize: { 
-  	width: 1024, 
-  	height: 600 
+    width: 1024, 
+    height: 600 
   },
   clipRect: {
-  	top: 0, 
-  	left: 0, 
-  	width: 1024, 
-  	height: 600 
+    top: 0, 
+    left: 0, 
+    width: 1024, 
+    height: 600 
   },
   zoomFactor: 0
 };
