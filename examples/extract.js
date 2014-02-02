92b1d35f6ce6d8a1d974bb9335251691f8ca8127
miniflycn <miniflycn@justany.net>
2014-02-02 19:18:49 +0800

    for coverage

--- examples/extract.js
+++ examples/extract.js
@@ -2,6 +2,7 @@ module.exports = (function () {
   "use strict"
   var urlExtract = require('../')();
 
+  // extract
   urlExtract.extract('http://www.baidu.com', function (job) {
     console.log('This is a extract example.');
     console.log(job);
