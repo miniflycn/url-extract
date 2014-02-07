33e5f69ff8b7b9317499ec62d88970720f676983
miniflycn <miniflycn@justany.net>
2014-02-07 20:01:19 +0800

    remove cache config

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 module.exports = {
   wsPort: 3001,
   workerNum: 0,
-  cache: 'object',
 
   maxJob: 100,
   maxQueueJob: 0,
-  maxCache: 10000,
   viewportSize: { 
     width: 1024, 
     height: 600 
